5b84b5aabac0f83b002a864f1ad9ed55be71223f
ymkmy98 <ymkmy98@gmail.com>
2023-07-16 23:55:02 +0900

    ヘッダーのスタイル修正

--- src/components/base/Header.jsx
+++ src/components/base/Header.jsx
@@ -1,11 +1,15 @@
-import Logo from '../elements/Logo'
-import Nav from '../elements/Nav'
+import React from 'react'
+import Logo from '@/components/elements/Logo'
+import Nav from '@/components/elements/Nav'
+import styles from '@/styles/base/header.module.css'
 
 export default function Header() {
   return (
   <header>
-    <Logo boxOn />
-    <Nav />
+    <div className={styles.flexContainer}>
+      <Logo boxOn />
+      <Nav />
+    </div>
   </header>
   )
 }
