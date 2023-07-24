177372f8c4c7717b8a7c4e5e876fb80d386ac5a5
ymkmy98 <ymkmy98@gmail.com>
2023-07-24 23:33:04 +0900

    記事詳細で前後の記事を表示するページネーションを追加

--- src/pages/blog/[slug]/index.jsx
+++ src/pages/blog/[slug]/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 // import { client } from 'libs/client'
 import { getPostBySlug, getAllPosts } from 'libs/api'
+import { prevNextPost } from 'libs/prevNextPost'
 import Container from '@/components/layouts/Container'
 import PostHeader from '@/components/elements/PostHeader'
 import PostBody from '@/components/layouts/PostBody'
@@ -8,7 +9,8 @@ import { TwoColumn } from '@/components/layouts/TwoColumn'
 import ConvertBody from '@/components/features/convert-body'
 import PostCategory from '@/components/elements/PostCategory'
 import { extractText } from '@/components/features/extract-text'
-import Meta from '@/components/Meta'
+// import Meta from '@/components/Meta'
+import Pagination from '@/components/elements/Pagination'
 import Image from 'next/legacy/image'
 // import { getPlaiceholder } from 'plaiceholder'
 
@@ -19,19 +21,21 @@ export default function Post({
   title,
   publish,
   content,
-  eyecatch,
   categories,
   description,
+  eyecatch,
+  prevPost,
+  nextPost,
 }) {
   return (
     <Container>
-      <Meta
+      {/* <Meta
         pgeTitle={title}
         pageDesc={description}
         pageImg={eyecatch.url}
         pageImgW={eyecatch.width}
         pageImgH={eyecatch.height}
-      />
+      /> */}
       <article>
         <PostHeader title={title} subTitle="Blog Article" publish={publish} />
 
@@ -58,38 +62,80 @@ export default function Post({
             <PostCategory categories={categories} />
           </TwoColumn.Sidebar>
         </TwoColumn>
+
+        <Pagination
+          prevText={prevPost.title}
+          prevUrl={`/blog/${prevPost.slug}`}
+          nextText={nextPost.title}
+          nextUrl={`/blog/${nextPost.slug}`}
+        />
       </article>
     </Container>
   )
 }
 
+export async function getStaticProps({ params }) {
+  const slug = params.slug
+  const post = await getPostBySlug(slug)
+
+  if (!post) {
+    return { notFound: true }
+  } else {
+    const { title, publishedAt: publish, content, categories } = post
+
+    const description = extractText(content)
+    const eyecatch = post.eyecatch ?? eyecatchlocal
+
+    // const { base64 } = await getPlaiceholder(eyecatch.url)
+    // eyecatch.blurDataURL = base64
+
+    const allPosts = await getAllPosts()
+    const [prevPost, nextPost] = prevNextPost(allPosts, slug)
+
+    return {
+      props: {
+        title,
+        publish,
+        content,
+        categories,
+        description,
+        eyecatch,
+        prevPost: prevPost,
+        nextPost: nextPost,
+      },
+    }
+  }
+}
+
 export async function getStaticPaths() {
-  const allSlugs = await getAllPosts()
+  const allPosts = await getAllPosts()
 
   return {
-    paths: allSlugs.map(({ slug }) => `/blog/${slug}`),
+    paths: allPosts.map(({ slug }) => `/blog/${slug}`),
     fallback: false,
   }
 }
 
-export async function getStaticProps(context) {
-  const slug = context.params.slug
+// export async function generateStaticParams() {
+//   const allPosts = await getAllPosts()
 
-  const post = await getPostBySlug(slug)
-  const description = extractText(post.content)
-  const eyecatch = post.eyecatch ?? eyecatchlocal
+//   return allPosts.map(({ post }) => {
+//     return {
+//       params: {
+//         slug: post,
+//       },
+//     }
+//   })
+// }
 
-  // const { base64 } = await getPlaiceholder(eyecatch.url)
-  // eyecatch.blurDataURL = base64
+// export async function generateStaticParams() {
+//   const allPosts = await getAllPosts()
 
-  return {
-    props: {
-      title: post.title,
-      publish: post.publishedAt,
-      content: post.content,
-      eyecatch: eyecatch,
-      categories: post.categories,
-      description: description,
-    },
-  }
-}
+//   return allPosts.map(({ post }) => {
+//     return {
+//       params: {
+//         slug: post,
+//       },
+//     }
+//   })
+// }
